f9d4942c6cfd6cde49451d6aef05eb7edd36bf2d
Daniel Demidko <demidko@farpost.com>
2020-08-20 02:24:16 +1000

    Имплементация структуры из псевдокода

--- Application.cs
+++ Application.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using VkNet;
 using VkNet.Model;
 using static Authorization;
 
 internal class Application
 {
+    private readonly VkApi _api;
     private readonly Dictionary<User, (DateTime timestamp, LinkedList<User> onlineFriends)>
         _db = new Dictionary<User, (DateTime, LinkedList<User>)>();
 
+    private Application(VkApi api)
+    {
+        _api = api;
+        // десериализация из файла
+    }
+    
     private void UpdateAsync()
     {
-        // dump there
+        
     }
 
     private void DumpAsync()
     {
-        throw new NotImplementedException();
+        // простая сериализация в файл
     }
 
     private void Analyze(User user)
