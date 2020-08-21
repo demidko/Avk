069b6d4ab1377ea7a9730361e721496d16e9f9b1
Daniel Demidko <demidko@farpost.com>
2020-08-21 20:23:52 +1000

    Упростил команды

--- Application.cs
+++ Application.cs
@@ -1,39 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
 using VkNet;
 using VkNet.Model;
+using static System.IO.File;
+using static System.Text.Json.JsonSerializer;
+using static System.TimeSpan;
 using static Authorization;
+using Timer = System.Timers.Timer;
 
+/// <summary>
+/// Главный класс
+/// </summary>
 internal class Application
 {
+    private const string DbPath = ".db";
+
+    private readonly Dictionary<User, LinkedList<(DateTime timestamp, List<User> onlineFriends)>>
+        _db = new Dictionary<User, LinkedList<(DateTime, List<User>)>>();
+
     private readonly VkApi _api;
-    private readonly Dictionary<User, (DateTime timestamp, LinkedList<User> onlineFriends)>
-        _db = new Dictionary<User, (DateTime, LinkedList<User>)>();
 
     private Application(VkApi api)
     {
         _api = api;
-        // десериализация из файла
-    }
-    
-    private void UpdateAsync()
-    {
-        
-    }
-
-    private void DumpAsync()
-    {
-        // простая сериализация в файл
+        if (Exists(DbPath))
+        {
+            _db = Deserialize<Dictionary<User, LinkedList<(DateTime, List<User>)>>>(ReadAllBytes(DbPath));
+        }
+        FromMinutes(1).Schedule(() => WriteAllBytesAsync(".db", SerializeToUtf8Bytes(_db)));
     }
 
-    private void Analyze(User user)
+    private void Analyze(string link)
     {
-        
     }
+
+    private void StopAnalysis
+
 
     private static void Main(string[] user)
     {
-        var api = Login(user);
+        var application = new Application(Login(user));
+        // цикл обработки команд
     }
-    
 }
---
+++ Utils.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Timers;
+
+/// <summary>
+/// Здесь хранятся вспомогательные методы
+/// </summary>
+internal static class Utils
+{
+    internal static void Schedule(this TimeSpan timeSpan, Action f) =>
+        new Timer(timeSpan.TotalMilliseconds) {AutoReset = true, Enabled = true}
+            .Elapsed += (s, e) => f.Invoke();
+}
